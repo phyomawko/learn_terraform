3ed2305b4d59728cb5d5e78f417f5ae392a805c2
phyomawko <pko65265@gmail.com>
2024-08-21 11:09:03 +0630

    Change the loop variable to object

--- variables.tf
+++ variables.tf
@@ -6,27 +6,46 @@ variable "vpc_cidr_block" {
 variable "vpc_name" {
   
 }
-variable "zones_for_public_subnets"{
-    type=list(string)
+# variable "zones_for_public_subnets"{
+#     type=list(string)
+    
+# }
+# variable "cidr_for_public_subnets"{
+#     type=list(string)
+    
+# }
+# variable "name_for_public_subnets"{
+#     type=list(string)
+    
+# }
+# variable "zones_for_private_subnets"{
+#     type=list(string)
+    
+# }
+# variable "cidr_for_private_subnets"{
+#     type=list(string)
+    
+# }
+# variable "name_for_private_subnets"{
+#     type=list(string)
+    
+# }
+variable public_subnets {
+    type= list(object({
+        cidr=string
+        zone=string
+        Name=string
+
+    }))
     
 }
-variable "cidr_for_public_subnets"{
-    type=list(string)
-    
-}
-variable "name_for_public_subnets"{
-    type=list(string)
-    
-}
-variable "zones_for_private_subnets"{
-    type=list(string)
-    
-}
-variable "cidr_for_private_subnets"{
-    type=list(string)
-    
-}
-variable "name_for_private_subnets"{
-    type=list(string)
+
+variable private_subnets {
+    type= list(object({
+        cidr=string
+        zone=string
+        Name=string
+
+    }))
     
 }
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,4 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+
 
 
 resource "aws_vpc" "PMKVPC" {
@@ -50,31 +48,31 @@ resource "aws_route_table" "PMKPrivateRT" {
 }
 resource "aws_subnet" "PMKPublicSubnets" {
   vpc_id = aws_vpc.PMKVPC.id
-  count=length(data.aws_availability_zones.available.names)
-  availability_zone = var.zones_for_public_subnets[count.index]
-  cidr_block = var.cidr_for_public_subnets[count.index]
+  count=length(var.public_subnets)
+  availability_zone = var.public_subnets[count.index].zone
+  cidr_block = var.public_subnets[count.index].cidr
   tags = {
-    Name=var.name_for_public_subnets[count.index]
+    Name=var.public_subnets[count.index].Name
   }
   
 }
 resource "aws_route_table_association" "PMKPublicRTAssociation" {
-  count=length(data.aws_availability_zones.available.names)
+  count=length(var.public_subnets)
   subnet_id      = aws_subnet.PMKPublicSubnets[count.index].id
   route_table_id = aws_route_table.PMKPublicRT.id
 }
 resource "aws_subnet" "PMKPrivateSubnets" {
   vpc_id = aws_vpc.PMKVPC.id
-  count=length(data.aws_availability_zones.available.names)
-  availability_zone = var.zones_for_private_subnets[count.index]
-  cidr_block = var.cidr_for_private_subnets[count.index]
+  count=length(var.private_subnets)
+  availability_zone = var.private_subnets[count.index].zone
+  cidr_block = var.private_subnets[count.index].cidr
   tags = {
-    Name=var.name_for_private_subnets[count.index]
+    Name=var.private_subnets[count.index].Name
   }
   
 }
 resource "aws_route_table_association" "PMKPrivateRTAssociation" {
-  count=length(data.aws_availability_zones.available.names)
+  count=length(var.private_subnets)
   subnet_id      = aws_subnet.PMKPrivateSubnets[count.index].id
   route_table_id = aws_route_table.PMKPrivateRT.id
 }
